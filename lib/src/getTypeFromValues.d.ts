42c471cb4b9a289a240b50b0fc7e8f5f964f46bb
Greg Diermann <greg@gregdiermann.com>
2020-11-05 20:13:47 -0600

    Changed husky config in package.json

--- lib/src/getTypeFromValues.d.ts
+++ lib/src/getTypeFromValues.d.ts
@@ -1,3 +1,2 @@
-import { GraphQLList } from 'graphql';
-declare const _default: (name: string, idArray?: string[], values?: any[], isRequired?: boolean) => import("graphql").GraphQLScalarType | import("graphql").GraphQLObjectType<any, any> | import("graphql").GraphQLInterfaceType | import("graphql").GraphQLUnionType | import("graphql").GraphQLEnumType | import("graphql").GraphQLInputObjectType | GraphQLList<any>;
+declare const _default: (name: string, idArray?: string[], values?: any[], isRequired?: boolean) => any;
 export default _default;
